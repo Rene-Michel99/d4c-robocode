ff6998291023beab01c222d0ab29952270cda3d8
Rene-Michel99 <renemichel856@gmail.com>
2021-08-23 16:24:56 -0300

    robo assassino

--- Dirty_deeds_done_dirt_cheap.java
+++ Dirty_deeds_done_dirt_cheap.java
@@ -1,10 +1,5 @@
-package laliho;
+package fodase;
 import robocode.*;
-import robocode.HitRobotEvent;
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
-import java.util.Random;
-
 import java.awt.*;
 
 //import java.awt.Color;
@@ -12,80 +7,68 @@ import java.awt.*;
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
 /**
- * Lolicon - a robot by (your name here)
+ * Dirty_deeds_done_dirt_cheap - a robot by (your name here)
  */
 
 
-public class Lolicon extends Robot {
-	int turnDirection = 1; // Clockwise or counterclockwise
-	Random gerador = new Random();
+public class Dirty_deeds_done_dirt_cheap extends AdvancedRobot {
+    int direction = 1;//which way to move
+    /**
+     * run:  Tracker's main run function
+     */
+    public void run() {
+        initialize();
+    }
+    public void initialize(){
+    	setAdjustRadarForRobotTurn(true);//keep the radar still while we turn
+        setBodyColor(Color.pink);
+        setGunColor(Color.gray);
+        setRadarColor(Color.cyan);
+        setScanColor(Color.white);
+        setBulletColor(Color.blue);
+        setAdjustGunForRobotTurn(true); // Keep the gun still when we turn
+        turnRadarRightRadians(999999999);//keep turning radar right
+    }
 
-	public void run() {
-		// Set colors
-		setBodyColor(Color.blue);
-		setGunColor(Color.red);
-		setRadarColor(Color.lightGray);
-		while (true) {
-			turnRight(5 * turnDirection);
-			anda();
-		}
-	}
-	public void anda(){
-		ahead(gerador.nextInt(26));
-		turnLeft(gerador.nextInt(90));
-	}
-	/**
-	 * onScannedRobot:  We have a target.  Go get it.
-	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		if (e.getBearing() >= 0) {
-			turnDirection = 1;
-		} else {
-			turnDirection = -1;
-		}
-
-		turnRight(e.getBearing());
-		fire(3);
-		scan(); // Might want to move ahead again!
-	}
-	
-	public void onHitByBullet(HitByBulletEvent e) {
-		turnRight(gerador.nextInt(90));
-		ahead(gerador.nextInt(30));
-	}
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		turnLeft(e.getBearing());
-	}
-
-	/**
-	 * onHitRobot:  Turn to face robot, fire hard, and ram him again!
-	 */
-	public void onHitRobot(HitRobotEvent e) {
-		if (e.getBearing() >= 0) {
-			turnDirection = 1;
-		} else {
-			turnDirection = -1;
-		}
-		turnRight(e.getBearing());
-
-		// Determine a shot that won't kill the robot...
-		// We want to ram him instead for bonus points
-		if (e.getEnergy() > 16) {
-			fire(3);
-		} else if (e.getEnergy() > 10) {
-			fire(2);
-		} else if (e.getEnergy() > 4) {
-			fire(1);
-		} else if (e.getEnergy() > 2) {
-			fire(.5);
-		} else if (e.getEnergy() > .4) {
-			fire(.1);
-		}
-		ahead(40); // Ram him again!
-	}
+    /**
+     * onScannedRobot:  Here's the good stuff
+     */
+    public void onScannedRobot(ScannedRobotEvent e) {
+        double absBearing = e.getBearingRadians()+getHeadingRadians();//enemies absolute bearing
+        double latVel=e.getVelocity() * Math.sin(e.getHeadingRadians() -absBearing);//enemies later velocity
+        double gunTurnAmt;//amount to turn our gun
+        setTurnRadarLeftRadians(getRadarTurnRemainingRadians());//lock on the radar
+        if(Math.random()>.9){
+            setMaxVelocity((12*Math.random())+12);//randomly change speed
+        }
+        if (e.getDistance() > 150) {//if distance is greater than 150
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/22);//amount to turn our gun, lead just a little bit
+            setTurnGunRightRadians(gunTurnAmt); //turn our gun
+            setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(absBearing-getHeadingRadians()+latVel/getVelocity()));//drive towards the enemies predicted future location
+            setAhead((e.getDistance() - 140)*direction);//move forward
+            setFire(3);//fire
+        }
+        else{//if we are close enough...
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/15);//amount to turn our gun, lead just a little bit
+            setTurnGunRightRadians(gunTurnAmt);//turn our gun
+            setTurnLeft(-90-e.getBearing()); //turn perpendicular to the enemy
+            setAhead((e.getDistance() - 140)*direction);//move forward
+            setFire(3);//fire
+        }
+    }
+    public void onHitWall(HitWallEvent e){
+        direction = -direction;//reverse direction upon hitting a wall
+    }
+    /**
+     * onWin:  Do a victory dance
+     */
+    public void onWin(WinEvent e) {
+        for (int i = 0; i < 50; i++) {
+            turnRight(30);
+            turnLeft(30);
+        }
+    }
 }
+
+
 
